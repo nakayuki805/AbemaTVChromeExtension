6eda5c75cefd2a9c4cb5f324cb97f385e31b7c02
pokecal <rse86028@nifty.com>
2016-04-29 23:05:49 +0900

    Update onairpage.js

--- onairpage.js
+++ onairpage.js
@@ -87,10 +87,10 @@ function moveComment(commentElement, commentLeftEnd){
 }
 function comeNG(prengcome){
     var ngedcome = prengcome;
-    ngedcome = ngedcome.replace(/[（\(][^）\)]*[∀дД＿\‿ω\﹃\_\ﾛ]?[^）\)]*[）\)]/g,"");
+    ngedcome = ngedcome.replace(/[Σ]?[ヾヽ]?[（\(][^）\)]*[∀дД▽△＿\‿ω\﹃\_\ﾛ]?[^）\)]*[）\)][ｼノ\/\ｼ\ﾉ]/g,"");
     ngedcome = ngedcome.replace(/(#\w+[　 ]*)+$/g,"");
     ngedcome = ngedcome.replace(/[ｗw]+/g,"ｗ");
-    ngedcome = ngedcome.replace(/[〜～ー－]+/g,"ー");
+    ngedcome = ngedcome.replace(/[〜～ー－━─]+/g,"ー");
     ngedcome = ngedcome.replace(/[・･…‥]+/g,"…");
     ngedcome = ngedcome.replace(/[　 \n]+/g," ");
     ngedcome = ngedcome.replace(/[？\?]+/g,"？");
