d7b7fad5aa71de3dcdb52160b5f8e041952d17c3
pokecal <rse86028@nifty.com>
2016-06-10 19:35:56 +0900

    新しい設定項目に対応

--- option.js
+++ option.js
@@ -1,6 +1,15 @@
 $(function(){
     $("#settingsArea").html(generateOptionHTML(true));
     $("#CommentMukouSettings").hide();
+    $("#CommentColorSettings").css("width","600px")
+      .css("background-color","darkgreen")
+      .css("padding","8px")
+      .children('div').css("clear","both")
+      .children('span.desc').css("padding","0px 4px")
+      .next('span.prop').css("background-color","white")
+      .css("padding","0px 4px")
+      .next('input[type="range"]').css("float","right")
+    ;
     chrome.storage.local.get(function (value) {
         var isResizeScreen = value.resizeScreen || false;
         console.log(value.movingCommentLimit)
@@ -27,7 +36,14 @@ $(function(){
         var isSureReadComment = value.sureReadComment || false;
         var sureReadRefreshx = Math.max(101,(value.sureReadRefreshx || 2000000));
         var isAlwaysShowPanel = value.isAlwaysShowPanel || false;
-        var isMovieResize = value.movieResize || false;
+//        var isMovieResize = value.movieResize || false;
+        var isMovieMaximize = value.movieMaximize || false;
+        var commentBackColor = value.commentBackColor || 255;
+        var commentBackTrans = value.commentBackTrans || 255;
+        var commentTextColor = value.commentTextColor || 0;
+        var commentTextTrans = value.commentTextTrans || 255;
+        var isCommentPadZero = value.commentPadZero || false;
+        var isCommentTBorder = value.commentTBorder || false;
         $("#isResizeScreen").prop("checked", isResizeScreen);
         $("#isDblFullscreen").prop("checked", isDblFullscreen);
         $("#isEnterSubmit").prop("checked", isEnterSubmit);
@@ -52,7 +68,27 @@ $(function(){
         $("#isSureReadComment").prop("checked", isSureReadComment);
         $("#sureReadRefreshx").val(sureReadRefreshx);
         $("#isAlwaysShowPanel").prop("checked", isAlwaysShowPanel);
-        $("#isMovieResize").prop("checked", isMovieResize);
+//        $("#isMovieResize").prop("checked", isMovieResize);
+        $("#isMovieMaximize").prop("checked", isMovieMaximize);
+        var bc="rgba("+commentBackColor+","+commentBackColor+","+commentBackColor+","+(commentBackTrans/255)+")";
+        var tc="rgba("+commentTextColor+","+commentTextColor+","+commentTextColor+","+(commentTextTrans/255)+")";
+        $("#CommentColorSettings>div>span.desc").css("background-color",bc)
+          .css("color",tc)
+        ;
+        $("#commentBackColor").val(commentBackColor)
+          .prev('span.prop').text(commentBackColor+" ("+Math.round(commentBackColor*100/255)+"%)")
+        ;
+        $("#commentBackTrans").val(commentBackTrans)
+          .prev('span.prop').text(commentBackTrans+" ("+Math.round(commentBackTrans*100/255)+"%)")
+        ;
+        $("#commentTextColor").val(commentTextColor)
+          .prev('span.prop').text(commentTextColor+" ("+Math.round(commentTextColor*100/255)+"%)")
+        ;
+        $("#commentTextTrans").val(commentTextTrans)
+          .prev('span.prop').text(commentTextTrans+" ("+Math.round(commentTextTrans*100/255)+"%)")
+        ;
+        $("#isCommentPadZero").prop("checked", isCommentPadZero);
+        $("#isCommentTBorder").prop("checked", isCommentTBorder);
     });
     $("#saveBtn").click(function () {
         chrome.storage.local.set({
@@ -80,10 +116,27 @@ $(function(){
             "sureReadComment": $("#isSureReadComment").prop("checked"),
             "sureReadRefreshx": Math.max(101,parseInt($("#sureReadRefreshx").val())),
             "isAlwaysShowPanel": $("#isAlwaysShowPanel").prop("checked"),
-            "movieResize": $("#isMovieResize").prop("checked")
+//            "movieResize": $("#isMovieResize").prop("checked")
+            "movieMaximize": $("#isMovieMaximize").prop("checked"),
+            "commentBackColor": parseInt($("#commentBackColor").val()),
+            "commentBackTrans": parseInt($("#commentBackTrans").val()),
+            "commentTextColor": parseInt($("#commentTextColor").val()),
+            "commentTextTrans": parseInt($("#commentTextTrans").val()),
+            "commentPadZero": $("#isCommentPadZero").prop("checked"),
+            "commentTBorder": $("#isCommentTBorder").prop("checked")
         }, function () {
             $("#info").show().text("設定保存しました").fadeOut(4000);
         });
+    });
+    $('#CommentColorSettings').change(function(){
+      var p=[];
+      $('#CommentColorSettings>div>input[type="range"]').each(function(i,jo){
+        $(jo).prev('span.prop').text($(jo).val()+" ("+Math.round($(jo).val()*100/255)+"%)");
+        p[i]=$(jo).val();
+      });
+      $('#CommentColorSettings>div>span.desc').css("background-color","rgba("+p[0]+","+p[0]+","+p[0]+","+(p[1]/255)+")")
+        .css("color","rgba("+p[2]+","+p[2]+","+p[2]+","+(p[3]/255)+")")
+      ;
     });
 });
 var keyinput = [];
