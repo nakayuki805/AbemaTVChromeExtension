5902f18119cee20bd5e4e8e2be383277c62ad6c7
nakayuki <yuki@nakayuki.net>
2016-07-02 19:49:45 +0900

    verup&説明変更

--- updatenotify.js
+++ updatenotify.js
@@ -2,9 +2,9 @@
 //webストア公開版のmanifest.jsonにはcontent_scriptsのjsの最後にこのファイルが加えられる
 //github版では不使用
 
-var currentUpdateNotifyVersion = 2;//2は0.0.20の時
+var currentUpdateNotifyVersion = 3;//3は0.0.21の時
 var optionUrl = chrome.extension.getURL("option.html");
-var notifyContent = "最近の更新で設定画面のがカテゴリー分けされ見やすくなりました。<br>また詳細な挙動設定など設定項目や機能も増えました。<br><b>この拡張機能の<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a>を開いて新たな機能を確認してみてください。</b><br>この拡張機能はAbemaTVの仕様変更によりある日突然うまく動かなくなることがあります。その際はアップデートをお待ちください。";
+var notifyContent = "現在、一部の環境でCM開始直前に画面が小さくなってしまう現象が確認されています。<br><b>この拡張機能の<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a>の下のある「コメント無効時関連リセット」ボタンを試してください。</b><br>設定リセとボタンはアップデートで新たに追加されました。";
 
 function updateInfo(message) {
     var toastElem = $("<div class='toast' id='updateInfo'><p>" + message + "<br><input type='button' value='閉じる' onclick='$(\"#updateInfo\").hide()' style='color:black;'></p></div>").appendTo("body");
