6f74c0feec5210a9fd163ddc3cc6e84191a858f5
nakayuki <yuki@nakayuki.net>
2016-07-03 20:26:33 +0900

    登録済み番組一覧を時刻順に&verup

--- prognotifies.js
+++ prognotifies.js
@@ -4,20 +4,27 @@ function dateToStr(date){
 }
 $(function(){
     chrome.storage.local.get(function(values){
-        var notifies = {};
+        var notifies = [];
         for (var key in values) {
             if (key.indexOf("progNotify")==0) {//通知登録データ
-                notifies[key] = values[key];
+                var eachNotify = values[key];
+                eachNotify.progNotifyName = key;
+                notifies.push(eachNotify);
             }
         }
+        //notifiesを時刻順にソート
+        notifies.sort(function(a, b){
+            return (a.programTime < b.programTime)?-1:1;
+        });
         //console.log(notifies)
-        for (var progNotifyName in notifies) {
-            var programUrl = "https://abema.tv/channels/"+notifies[progNotifyName].channel+"/slots/"+notifies[progNotifyName].programID;
+        for (var i=0; i<notifies.length; i++) {
+            var progNotifyName = notifies[i].progNotifyName;
+            var programUrl = "https://abema.tv/channels/"+notifies[i].channel+"/slots/"+notifies[i].programID;
             var trhtml = "<tr id=\"tr_"+progNotifyName+"\">";
-            trhtml += "<td>"+notifies[progNotifyName].programTitle+"</td>";
-            trhtml += "<td>"+notifies[progNotifyName].channelName+"</td>";
-            trhtml += "<td><a href=\""+programUrl+"\" target=\"_blank\">"+notifies[progNotifyName].programID+"</a></td>";
-            trhtml += "<td>"+dateToStr(notifies[progNotifyName].programTime)+"</td>";
+            trhtml += "<td>"+notifies[i].programTitle+"</td>";
+            trhtml += "<td>"+notifies[i].channelName+"</td>";
+            trhtml += "<td><a href=\""+programUrl+"\" target=\"_blank\">"+notifies[i].programID+"</a></td>";
+            trhtml += "<td>"+dateToStr(notifies[i].programTime)+"</td>";
             trhtml += "<td><input type='button' value='削除' id='delbtn_"+progNotifyName+"'></td>";
             trhtml += "</tr>"
             $("#notifyProgTable tbody").append(trhtml);
