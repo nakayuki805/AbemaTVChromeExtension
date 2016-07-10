2292b46e022482f890d480922eb86ce952d543d5
nakayuki <yuki@nakayuki.net>
2016-07-11 01:51:54 +0900

    verup

--- updatenotify.js
+++ updatenotify.js
@@ -2,15 +2,15 @@
 //webストア公開版のmanifest.jsonにはcontent_scriptsのjsの最後にこのファイルが加えられる
 //github版では不使用
 
-var currentUpdateNotifyVersion = 3;//3は0.0.21の時
+var currentUpdateNotifyVersion = 4;//4は0.1.2の時
 var optionUrl = chrome.extension.getURL("option.html");
-var notifyContent = "現在、一部の環境でCM開始直前に画面が小さくなってしまう現象が確認されています。<br><b>この拡張機能の<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a>の下のある「コメント無効時関連リセット」ボタンを試してください。</b><br>設定リセットボタンはアップデートで新たに追加されました。";
+var notifyContent = "新着コメントを少し目立たせる機能(設定で有効)が追加されました。<br>abematv拡張機能の実験的なfirefox版<a href='https://www.nakayuki.net/abema-ext/' target='_blank'>公開中</a>です。<br>再掲:CM開始直前に画面が小さくなってしまう場合はこの拡張機能の<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a>の下のある「コメント無効時関連リセット」ボタンを試してください。";
 
 function updateInfo(message) {
     var toastElem = $("<div class='toast' id='updateInfo'><p>" + message + "<br><input type='button' value='閉じる' onclick='$(\"#updateInfo\").hide()' style='color:black;'></p></div>").appendTo("body");
     setTimeout(function(){
         toastElem.fadeOut(10000);
-    },30000);
+    },20000);
 }
 chrome.storage.local.get("updateNotifyVer", function (value) {
     if (value.updateNotifyVer == undefined || value.updateNotifyVer < currentUpdateNotifyVersion) {
