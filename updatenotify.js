1ea94b93f5010943fe848d40d4365c4e09e10786
nakayuki <yuki@nakayuki.net>
2016-07-21 22:49:56 +0900

    説明微修正(typo)

--- updatenotify.js
+++ updatenotify.js
@@ -1,10 +1,10 @@
 //webストア版向けに拡張機能の新機能等お知らせをアップデート後初回のみtoast状に表示
-//webストア公開版のmanifest.jsonにはcontent_scriptsのjsの最後にこのファイルが加えられる
+//webストア公開版、firefoxパッケージ版のmanifest.jsonにはcontent_scriptsのjsの最後にこのファイルが加えられる
 //github版では不使用
 
 var currentUpdateNotifyVersion = 5;//4は0.2.1の時
 var optionUrl = chrome.extension.getURL("option.html");
-var notifyContent = "最近追加された機能(デフォルト無効):「Twitterで通知を受け取る」放送画面の「今日のみどころ」をの非表示オプション、番組表の表示改善<br>必要に応じて有効にしてください→<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a><br>abematv拡張機能の実験的なfirefox版<a href='https://www.nakayuki.net/abema-ext/' target='_blank'>公開中</a>です。";
+var notifyContent = "最近追加された機能(デフォルト無効):「Twitterで通知を受け取る」放送画面の「今日のみどころ」の非表示オプション、番組表の表示改善<br>必要に応じて有効にしてください→<a href='"+optionUrl+"' target='_blank'>オプション設定画面</a><br>abematv拡張機能の実験的なfirefox版<a href='https://www.nakayuki.net/abema-ext/' target='_blank'>公開中</a>です。";
 
 function updateInfo(message) {
     var toastElem = $("<div class='toast' id='updateInfo'><p>" + message + "<br><input type='button' value='閉じる' onclick='$(\"#updateInfo\").hide()' style='color:black;'></p></div>").appendTo("body");
