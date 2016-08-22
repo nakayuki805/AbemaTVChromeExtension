118bbd59b914c24154262ccc95cb7bbe4bef505f
pokecal <rse86028@nifty.com>
2016-08-22 23:25:29 +0900

    Patch 1 (#58)
    * Update onairpage.js
    
    * Update option.js
    
    * Update settings.js

--- settings.js
+++ settings.js
@@ -160,7 +160,14 @@ var settingsList = [
             },
             {
                 "name": "isComelistClickNG",
-                "description": "コメント一覧クリックでNG追加欄を表示(このNG追加欄による保存は一時的です。永久保存は設定画面へ。)",
+//                "description": "コメント一覧クリックでNG追加欄を表示(このNG追加欄による保存は一時的です。永久保存は設定画面へ。)",
+                "description": "コメント一覧クリックでNG追加欄を表示(NGボタン1回クリックで一時保存(黄色)、短時間で2回クリックすると永久保存(赤色)になります。)",
+                "type": "boolean",
+                "isInstantChangable": true
+            },
+            {
+                "name": "isComeClickNGautoClose",
+                "description": "↑でNG登録後、自動的にNG追加欄を閉じる",
                 "type": "boolean",
                 "isInstantChangable": true
             },
